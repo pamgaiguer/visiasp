3b2a50b562fd93dde76c7f73c98d85af9e1dca0a
Austin Felipe <austinfelipe@ig.com.br>
2014-08-01 01:28:10 -0300

    Imagem Projeto class

---
+++ Visi/Models/ImagemProjeto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Visi
+{
+	public class ImagemProjeto
+	{
+		public string Id {
+			get;
+			set;
+		}
+
+		public string UrlNormal {
+			get;
+			set;
+		}
+
+		public string UrlPequena {
+			get;
+			set;
+		}
+
+		public string Nome {
+			get;
+			set;
+		}
+
+		public string Local {
+			get;
+			set;
+		}
+
+		public string Classificadores {
+			get;
+			set;
+		}
+
+		public ImagemProjeto ()
+		{
+
+		}
+	}
+}
+
