e405b4c27dfd73fae418e64d964fba74e721dbdc
AustinFelipe <austinfelipe@ig.com.br>
2014-08-05 14:58:07 -0300

    Projetos route included

--- VisiProj/App_Start/RouteConfig.cs
+++ VisiProj/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace VisiProj
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Projetos",
+                url: "projetos/{id}",
+                defaults: new { controller = "Projetos", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Welcome", action = "Index", id = UrlParameter.Optional }
