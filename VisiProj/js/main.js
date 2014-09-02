01e173ba82114b99e18010e7abefffe28622fc36
AustinFelipe <austinfelipe@ig.com.br>
2014-09-02 19:33:59 -0300

    Adicionado prototipo para filtro de fotos via client

---
+++ VisiProj/js/main.js
@@ -0,0 +1,45 @@
+﻿/// <reference path="_reference.js" />
+$(function () {
+
+    'use strict';
+
+    // Menu lateral esquerdo (Categorias)
+    var $menu = $('#menuCat');
+
+    var onClickMenu = function (e) {
+
+        // Pegamos o anchor clicado e subidos um nivel para pegar o li
+        var $el = $(e.target).closest('li');
+
+        // Pega a categoria do [data-categoria]
+        var cat = $el.attr('data-categoria');
+
+        // Cancela acao padrao do anchor
+        e.preventDefault();
+
+        // Suma daqui :)
+        $menu.find('.projectList').hide(200);
+
+        // Remove todas as classes dos ul
+        $menu.children('li').removeClass('active');
+
+        // Coloca a classe no li
+        $el.addClass('active');
+
+        // Procura a ul filha do elemento clicado pelo id (cat + numeroCat)
+        var itens = $menu.find('ul#cat' + cat);
+
+        // Caso itens seja vazio, nao continua a acao (Pode ser o Todos, que nao possui filhos)
+        if (itens == undefined) {
+            return;
+        }
+
+        // Mostra menu filho com animacao
+        itens.show(200);
+
+    }
+
+    // Bind de eventos
+    $menu.on('click', onClickMenu);
+
+});
