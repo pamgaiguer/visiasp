2bb79c9af95248f353976b888f50eca929c3b2b2
Pamella Gaiguer <pamella.gaiguer@gmail.com>
2014-11-30 12:49:37 -0200

    melhorando querys base

---
+++ VisiProj/querys/select_nomes_imagens2.sql
@@ -0,0 +1,12 @@
+﻿--SELECT * FROM INFORMATION_SCHEMA.TABLES;
+select * from CategoriaModels;
+select * from ImagemProjetoModels;
+select * from ProjetoModels;
+select ProjetoModels.Id, ProjetoModels.Nome, ProjetoModels.Local, ProjetoModels.Area, 
+ImagemProjetoModels.UrlNormal, ImagemProjetoModels.UrlMiniatura, ProjetoModels.Deleted
+FROM ProjetoModels 
+INNER JOIN ImagemProjetoModels ON ProjetoModels.Id = ImagemProjetoModels.Projeto_Id
+WHERE ProjetoModels.deleted = 0
+order by ProjetoModels.Nome;
+
+select * from ImagemProjetoModels where TipoImagem = 1;
